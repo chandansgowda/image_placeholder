2ecd519357cb11b07a1ab63a785f839c0edf2fd6
chandansgowda <chandansuresh007@gmail.com>
2023-02-04 12:18:24 +0530

    Initial commit

---
+++ lib/image_placeholder.dart
@@ -0,0 +1,14 @@
+library image_placeholder;
+
+import 'package:flutter/material.dart';
+
+class ImagePlaceholder extends StatelessWidget {
+  const ImagePlaceholder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO: Implement image placeholder widget here
+    return Container();
+  }
+}
+
