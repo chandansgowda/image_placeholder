c2117cee4a4a27c0a2cc6597256875196475d04f
M Madhav Reddy <98480251+M-Madhavreddy@users.noreply.github.com>
2023-02-04 12:54:05 +0530

    copied imagePlaceholder widget

--- lib/image_placeholder.dart
+++ lib/image_placeholder.dart
@@ -1,14 +1,41 @@
-library image_placeholder;
-
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class ImagePlaceholder extends StatelessWidget {
-  const ImagePlaceholder({Key? key}) : super(key: key);
+  int width;
+  int height;
+  Color backgroundColor;
+  Color textColor;
+  String? text;
+  String? formatType;
 
-  @override
+  ImagePlaceholder(
+      {required this.width,
+      required this.height,
+      this.backgroundColor = Colors.white,
+      this.textColor = Colors.black,
+      this.formatType = 'png',
+      this.text});
+
   Widget build(BuildContext context) {
-    //TODO: Implement image placeholder widget here
-    return Container();
+    String backgroundColor1 = backgroundColor.toHex().replaceAll('ff', '');
+    String textColor1 = textColor.toHex().replaceAll('ff', '');
+
+    return text == null
+        ? Image.network(
+            'https://via.placeholder.com/$width x $height.$formatType/$backgroundColor1/$textColor1')
+        : Image.network(
+            'https://via.placeholder.com/$width x $height.$formatType/$backgroundColor1/$textColor1?text=$text');
   }
+}
+
+extension HexColor on Color {
+  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
+  String toHex({bool leadingHashSign = false}) =>
+      '${leadingHashSign ? '#' : ''}'
+      '${alpha.toRadixString(16).padLeft(2, '0')}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
 }
 
